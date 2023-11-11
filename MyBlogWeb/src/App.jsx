0c3947d2da43be2e25177b186475146960d4272c
Swarnendu <swarnendumaity633@gmail.com>
2023-11-11 22:52:04 +0530

    myBlogApp

--- MyBlogWeb/src/App.jsx
+++ MyBlogWeb/src/App.jsx
@@ -1,9 +1,9 @@
- 
-import { useEffect, useState } from 'react'
-import {useDispatch} from 'react-redux'
-import './App.css'
-import authService from './appwrite/auth'
-import {login ,logout} from './store/authSlice'
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import {Header ,Footer} from './component'
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -15,18 +15,24 @@ function App() {
       if ( userData) {
         dispatch(login({userData}))
       }else {
-        dispatch(logout({userData}))
+        dispatch(logout())
       }
     })
-    .finally()
+    .finally(()=> setLoading(false))
   },[])
-  return (
-    <> 
-    
-    <h1>Hello </h1>
- 
-    </>
-  )
+
+   return !loading ? (
+    <div className='min-h-screen flex flex-wrap
+    content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/> 
+      <main>
+
+      </main>
+      <Footer/> 
+      </div>
+    </div>
+   ) : null
 }
 
 export default App
--- MyBlogWeb/src/component/Header/Header.jsx
+++ MyBlogWeb/src/component/Header/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Container,Logo,LogoutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+
+  const authStatus = useSelector((state)=> state.auth.status)
+  
   return (
     <div>
       
