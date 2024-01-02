7101d6e0a220b456a7dcfa7acfd670da01496b13
Swarnendu <swarnendumaity633@gmail.com>
2024-01-02 11:03:25 +0530

    Assignment

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
-import {motion} from 'framer-motion'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
 
-import {styles} from '../styles'
-import {services} from '../constants'
-import {fadeIn, textVariant} from '../utils/motion'
-import {SectionWrapper} from '../hoc'
-
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -36,19 +35,15 @@ const ServiceCard = ({ index, title, icon }) => (
   </Tilt>
 );
 
-
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>
-       Introduction
-      </p>
-      <h2 className={styles.sectionHeadText}>
-        Overview
-      </h2>
-    </motion.div>
-    <motion.p
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
@@ -58,14 +53,14 @@ const About = () => {
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
+
       <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index)=>(
-          <ServiceCard key = {service.title} index={index}
-          {...service}/>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper (About,'about')
+export default SectionWrapper(About, "about");
