3b5c4ad3db686242396523d07114c450f14736b3
Swarnendu <swarnendumaity633@gmail.com>
2024-01-05 17:27:02 +0530

    Projects

---
+++ MyBlogWeb/src/pages/AllPosts.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import appwriteService from '../appwrite/config'
+
+
+
+function AllPosts() {
+    const [post,setPost]= useState();
+    useEffect(()=>{},[]);
+    appwriteService.getPost([]).then((post)=>{
+        if(post){
+            setPost(post.documents)
+        }
+    });
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+                </Container>
+        </div>
+      )
+    }
+    
+    export default AllPosts
